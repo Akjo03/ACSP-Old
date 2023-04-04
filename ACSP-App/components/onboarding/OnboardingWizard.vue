f13b93d8d751bc824a8ef24617de071414e0349b
Akjo03 <lukas.kueffer@outlook.com>
2023-04-04 15:20:16 +0200

    Changed some visuals for onboarding

--- ACSP-App/components/onboarding/OnboardingWizard.vue
+++ ACSP-App/components/onboarding/OnboardingWizard.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="flex flex-col py-4 items-center">
-        <div class="flex flex-row w-[50%] h-6 md:h-8 rounded-2xl shadow-2xl bg-white/5 ring-1 ring-white/10">
+        <div class="flex flex-row w-[50%] max-w-2xl h-6 md:h-8 rounded-2xl shadow-2xl bg-white/5 ring-1 ring-white/10">
+            <div v-for="page in pages" :key="page.id" class="flex flex-row flex-1 px-4 my-4">
+                <div class="flex flex-col items-center justify-center flex-1">
 
+                </div>
+            </div>
         </div>
     </div>
     <div class="flex flex-1">
@@ -14,12 +18,14 @@ import {ref, computed} from 'vue'
 import OnboardingLanguagePage from './pages/OnboardingLanguagePage.vue'
 
 type Page = {
+    index: number,
     id: string,
     component: any,
+    completed?: boolean
 }
 
 const pages: Page[] = [
-    {id: 'languageSelect', component: OnboardingLanguagePage}
+    {index: 1, id: 'languageSelect', component: OnboardingLanguagePage}
 ]
 
 const currentPage = ref('languageSelect')
--- ACSP-App/components/onboarding/pages/OnboardingLanguagePage.vue
+++ ACSP-App/components/onboarding/pages/OnboardingLanguagePage.vue
@@ -1,12 +1,10 @@
 <template>
     <div class="flex flex-col flex-grow">
-        <div class="sm:mx-auto sm:w-full sm:max-w-md py-6">
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
             <h2 class="mt-12 text-center text-2xl lg:text-3xl font-semibold tracking-tight text-themeText">Language / Sprache</h2>
         </div>
         <div class="flex flex-col items-center flex-grow">
-            <div class="flex flex-col w-[80%] h-[70%] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10">
 
-            </div>
         </div>
     </div>
 </template>
