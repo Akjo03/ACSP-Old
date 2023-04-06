5b6b30a859512048808d7ece0ed5e3b3709dc83a
Akjo03 <lukas.kueffer@outlook.com>
2023-04-06 23:31:39 +0200

    Cleaned up pages

--- ACSP-App/pages/dashboard.vue
+++ ACSP-App/pages/dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="text-3xl font-semibold text-themeText text-center pt-4">Dashboard</h1>
+
 </template>
 
 <script setup lang="ts">
--- ACSP-App/pages/index.vue
+++ ACSP-App/pages/index.vue
@@ -3,6 +3,5 @@
 </template>
 
 <script setup lang="ts">
-
 import HeroSection from "../components/index/HeroSection.vue";
 </script>
